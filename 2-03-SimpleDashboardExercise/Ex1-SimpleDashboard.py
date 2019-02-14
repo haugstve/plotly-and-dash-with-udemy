678e3f22b3012a14c7387ea73f13030ef3f9171f
haugstve <daniel.haugstvedt@gmail.com>
2019-02-14 14:52:37 +0100

    Stated solving the first dash excersice

--- 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
+++ 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
@@ -8,31 +8,26 @@
 ######
 
 # Perform imports here:
-
-
-
-
+#import dash
+#import dash_core_components as dcc
+#import dash_html_components as html
+import plotly.graph_objs as go 
+import plotly.offline as pyo
+import pandas as pd
 
 
 # Launch the application:
-
+#app = dash.Dash()
 
 # Create a DataFrame from the .csv file:
-
+df = pd.read_csv('../Data/OldFaithful.csv')
 
 # Create a Dash layout that contains a Graph component:
+data = [go.Scatter(x=df['X'],y=df['Y'],mode='markers')]
+layout = go.Layout(title = 'title')
 
-
-
-
-
-
-
-
-
-
-
-
+fig = go.Figure(data = data, layout = layout)
+pyo.plot(fig)
 
 
 
